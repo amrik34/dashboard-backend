557bddd663c24175712db1c4c8453433434f87ba
amrik34 <amrik3412singh@gmail.com>
2023-04-05 23:00:19 +0530

    mongos issues

--- db/config.js
+++ db/config.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
-const DB =
-  "mongodb+srv://amrik3412singh:quiO3hubyDP7nULG@cluster0.t0tqxz5.mongodb.net/mernStack?retryWrites=true&w=majority";
-
+// const DB =
+// "mongodb+srv://amrik3412singh:quiO3hubyDP7nULG@cluster0.t0tqxz5.mongodb.net/mernStack?retryWrites=true&w=majority";
+const DB = process.env.MONGODB_URI;
 mongoose
   // .connect("mongodb://127.0.0.1/e-commerce")
-  .connect(DB)
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("Database Connected Successfully"))
   .catch((err) => console.log(err));
